f278ca7ca75cabe19d82014c5dd7bd567f307fb0
andberg <andreaberglund@hotmail.com>
2017-06-14 22:16:09 +0200

    Structured the story with chapters. Also Changed classname with backgroundcolor to change background-color and color.

--- kabbeleka.ink
+++ kabbeleka.ink
@@ -1,3 +1,6 @@
+INCLUDE kapitel/kapitel1
+INCLUDE kapitel/kapitel2
+
 -> kabbeleka
 
 === kabbeleka ===
@@ -5,29 +8,9 @@
 # img:Logotyp.png
 # wait:1500
 # img:Logotyp-kabbeleka.png
-
 # wait:1500
+
 *	Kapitel 1
     -> kapitel1
 
-	
-=== kapitel1 ===
-# wait:1000
-# video:Bilresa.mp4
-# wait: 125000
 
-*   Kapitel 2
-    -> kapitel2
-
-=== kapitel2 ===
-
-Ljudet av bilen som kör upp på en grusväg. Tove kliver ut, öppnar bagageluckan och plockar ut sin väska. Samtidigt som bagageluckan stängs hörs ljudet från en dörr öppnas en liten bit därifrån.
-
-"Men underbara du, vad sen du blev. Det måste ha varit en förfärligt lång resa. Kom in, jag har förberett fika."
-
-* "Tack farmor, jag kommer."
-    -> kapitel2del1
-    
-=== kapitel2del1 ===
-
-    ->DONE
--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-(function(storyContent) {
+(function (storyContent) {
 
     var story = new inkjs.Story(storyContent);
-
-    var storyContainer = document.querySelectorAll('#story')[0];
+    var storyContainer = document.querySelectorAll("#story")[0];
 
     function showAfter(delay, el) {
-        setTimeout(function() { el.classList.add("show") }, delay);
+        setTimeout(function () {
+            el.classList.add("show")
+        }, delay);
     }
 
     function scrollToBottom() {
         var progress = 0.0;
         var start = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
         var dist = document.body.scrollHeight - window.innerHeight - start;
-        if( dist < 0 ) return;
+        if (dist < 0) return;
 
-        var duration = 300 + 300*dist/100;
+        var duration = 300 + 300 * dist / 100;
         var startTime = null;
+
         function step(time) {
-            if( startTime == null ) startTime = time;
-            var t = (time-startTime) / duration;
-            var lerp = 3*t*t - 2*t*t*t;
-            window.scrollTo(0, start + lerp*dist);
-            if( t < 1 ) requestAnimationFrame(step);
+            if (startTime == null) startTime = time;
+            var t = (time - startTime) / duration;
+            var lerp = 3 * t * t - 2 * t * t * t;
+            window.scrollTo(0, start + lerp * dist);
+            if (t < 1) requestAnimationFrame(step);
         }
         requestAnimationFrame(step);
     }
 
-    function ProcessTag(tag, delay)
-    {
-        if (tag.indexOf("img:") == 0)
-        {
+    function ProcessTag(tag, delay) {
+        if (tag.indexOf("img:") == 0) {
             fileName = tag.substr(4);
-            var element = document.createElement('img');
+            var element = document.createElement("img");
             element.src = fileName;
             storyContainer.appendChild(element);
             showAfter(delay, element);
@@ -39,7 +39,8 @@
 
         if (tag.indexOf("video:") == 0) {
             fileName = tag.substr(6);
-            var element = document.createElement('video');
+            var element = document.createElement("video");
+            element.controls = true;
             element.autoplay = true;
             element.src = fileName;
             storyContainer.appendChild(element);
@@ -48,12 +49,39 @@
 
         if (tag.indexOf("backgroundColor:") == 0) {
             color = tag.substr(16);
-            setTimeout(function () { document.getElementsByTagName('body')[0].style.backgroundColor = color }, delay);
+            if (color.indexOf("dark") !== -1) {
+                setTimeout(function () {
+                    document.getElementsByTagName("body")[0].classList.add("dark");
+                }, delay);
+            } else {
+                setTimeout(function () {
+                    document.getElementsByTagName("body")[0].classList.remove("dark")
+                }, delay);
+            }
+
+        }
+
+        if (tag.indexOf("fontColor:") == 0) {
+            color = tag.substr(16);
+            console.log(color);
+            setTimeout(function () {
+                document.getElementsByTagName("body")[0].style.color = color;
+            }, delay);
         }
 
         if (tag.indexOf("url:") == 0) {
             url = tag.substr(4);
-            setTimeout(function () { window.open(url) }, delay);
+            setTimeout(function () {
+                window.open(url)
+            }, delay);
+        }
+
+        if (tag.indexOf("clear") == 0) {
+            var existingContent = storyContainer.querySelectorAll("p, a, img, video");
+            for (var i = 0; i < existingContent.length; i++) {
+                var c = existingContent[i];
+                c.parentNode.removeChild(c);
+            }
         }
     }
 
@@ -63,21 +91,17 @@
         var delay = 0.0;
 
         // Generate story text - loop through available content
-        while(story.canContinue) {
+        while (story.canContinue) {
 
             // Get ink to generate the next paragraph
             var paragraphText = story.Continue();
 
             // Process Tags
-            story.currentTags.forEach(function (tag)
-            {
-                if (tag.indexOf("wait:") == 0)
-                {
+            story.currentTags.forEach(function (tag) {
+                if (tag.indexOf("wait:") == 0) {
                     duration = tag.substr(5);
                     delay += Number(duration);
-                }
-                else
-                {
+                } else {
                     ProcessTag(tag, delay);
                 }
 
@@ -85,21 +109,21 @@
             });
 
             // Create paragraph element
-            var paragraphElement = document.createElement('p');
+            var paragraphElement = document.createElement("p");
             paragraphElement.innerHTML = paragraphText;
             storyContainer.appendChild(paragraphElement);
 
             // Fade in paragraph after a short delay
             showAfter(delay, paragraphElement);
-
             delay += 200.0;
+
         }
 
         // Create HTML choices from ink choices
-        story.currentChoices.forEach(function(choice) {
+        story.currentChoices.forEach(function (choice) {
 
             // Create paragraph with anchor element
-            var choiceParagraphElement = document.createElement('p');
+            var choiceParagraphElement = document.createElement("p");
             choiceParagraphElement.classList.add("choice");
             choiceParagraphElement.innerHTML = `<a href='#'>${choice.text}</a>`
             storyContainer.appendChild(choiceParagraphElement);
@@ -110,14 +134,14 @@
 
             // Click on choice
             var choiceAnchorEl = choiceParagraphElement.querySelectorAll("a")[0];
-            choiceAnchorEl.addEventListener("click", function(event) {
+            choiceAnchorEl.addEventListener("click", function (event) {
 
                 // Don't follow <a> link
                 event.preventDefault();
 
                 // Remove all existing choices
-                var existingChoices = storyContainer.querySelectorAll('p.choice');
-                for(var i=0; i<existingChoices.length; i++) {
+                var existingChoices = storyContainer.querySelectorAll("p.choice");
+                for (var i = 0; i < existingChoices.length; i++) {
                     var c = existingChoices[i];
                     c.parentNode.removeChild(c);
                 }
